c72eed199526295587602fad2b4e42cc4c33e09a
audu paul <audupaul58@gmail.com>
2022-06-09 13:32:10 +0100

    New updates for Heroku deployment

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qt18ip17hpkbwz=nevxqmts0t-dkb3s!(f&jhtubjo9n(43w7(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
--- post/tests.py
+++ post/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import PostModel
 
 # Create your tests here.
+
+class PostModelTEST(TestCase):
+    pass
+
+class HomePageTest(TestCase):
+    pass
